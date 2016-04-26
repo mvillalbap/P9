3655de2d54d5f50561b7e624cc6b7c2f20311aea
mvillalbap <miguel.villalba.perez@alumnos.upm.es>
2016-04-26 17:09:36 +0200

    Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var control = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
+
+router.get('/question', control.question);
+router.get('/check', control.check);
 
 module.exports = router;
