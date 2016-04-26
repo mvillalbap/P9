09a2618366e2a1ef1bc413eab5d06759477f57bb
mvillalbap <miguel.villalba.perez@alumnos.upm.es>
2016-04-26 19:25:07 +0200

    Créditos y enlace a GitHub

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,9 @@ var control = require('../controllers/quiz_controller');
 router.get('/', function(req, res, next) {
   res.render('index');
 });
+router.get('/author', function(req, res, next) {
+  res.render('author')
+});
 
 router.get('/question', control.question);
 router.get('/check', control.check);
