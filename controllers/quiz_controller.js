865475bb6b2f14bc38102c06df9c07cafdb7530c
mvillalbap <miguel.villalba.perez@alumnos.upm.es>
2016-04-26 17:17:42 +0200

    No perder respuesta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,8 +1,14 @@
 exports.question = function(req,res,next){
-	res.render('quizzes/question', {question: 'Capital de Italia'});
+	var answer = req.query.answer || '';
+	res.render('quizzes/question', {question: 'Capital de Italia',
+									answer: answer}
+	);
 };
 
 exports.check = function(req,res,next){
+	var answer = req.query.answer || '';
 	var result = req.query.answer === 'Roma'? 'Correcta' : 'Incorrecta';
-	res.render('quizzes/result', {result: result});
+	res.render('quizzes/result', {result: result,
+									answer: answer}
+	);
 }
