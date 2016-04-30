a269867ce5e7032bb3023a8a814ce6c9a86b5448
mvillalbap <miguel.villalba.perez@alumnos.upm.es>
2016-04-30 09:11:28 +0200

    La base de datos

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,14 +1,38 @@
+var models = require('../models');
+
 exports.question = function(req,res,next){
-	var answer = req.query.answer || '';
-	res.render('quizzes/question', {question: 'Capital de Italia',
-									answer: answer}
-	);
+	models
+	.Quiz
+	.findOne()
+	.then(function(quiz){
+		if(quiz){
+			var answer = req.query.answer || '';
+			res
+			.render('quizzes/question', {question: quiz.question, answer: answer});
+		}else{
+			throw new Error('No hay preguntas en la base de datos.');
+		}
+	}).catch(function(error){
+		next(error);
+	});
 };
 
 exports.check = function(req,res,next){
-	var answer = req.query.answer || '';
-	var result = req.query.answer === 'Roma'? 'Correcta' : 'Incorrecta';
-	res.render('quizzes/result', {result: result,
-									answer: answer}
-	);
-}
+	models
+	.Quiz
+	.findOne()
+	.then(function(quiz){
+		if(quiz){
+			var answer = req.query.answer || '';
+			answer = answer.trim();
+			var exp = RegExp('^' + quiz.answer + '$','i');
+			var result = answer.match(exp)? 'Correcta' : 'Incorrecta';
+			res
+			.render('quizzes/result', {result: result, answer: answer});
+		}else{
+			throw new Error('No hay preguntas en la base de datos.');
+		}
+	}).catch(function(error){
+		next(error);
+	});
+};
