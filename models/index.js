c134807ab3ab288177c612c7b951044771a78248
mvillalbap <miguel.villalba.perez@alumnos.upm.es>
2016-05-01 09:35:39 +0200

    Despliegue BBDD en Heroku

--- models/index.js
+++ models/index.js
@@ -2,8 +2,24 @@ var path = require('path');
 
 var Sequelize = require('sequelize');
 
-var sequelize = new Sequelize(null, null, null,
-						{dialect: 'sqlite', storage: 'quiz.sqlite'});
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+
+var DATABASE_PROTOCOL = url[1];
+var DATABASE_DIALECT = url[1];
+var DATABASE_USER = url[2];
+var DATABASE_PASSWORD = url[3];
+var DATABASE_HOST = url[4];
+var DATABASE_PORT = url[5];
+var DATABASE_NAME = url[6];
+var DATABASE_STORAGE = process.env.DATABASE_STORAGE;
+
+var sequelize = new Sequelize(DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD,
+						{dialect: DATABASE_DIALECT, 
+							protocol: DATABASE_PROTOCOL,
+							port: DATABASE_PORT,
+							host: DATABASE_HOST,
+							storage: DATABASE_STORAGE,
+							omit: true});
 
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 
